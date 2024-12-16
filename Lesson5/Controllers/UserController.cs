4d7c4297352a2533ed3f34e8eb0e15b0996a6a0f
Тарас Семенів <tarassemeniv2@gmail.com>
2024-12-16 18:53:42 +0100

    Add project files.

---
+++ Lesson5/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Lesson5.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace Lesson5.Controllers;
+[ApiController]
+[Route("users")]
+public class UserController : Controller
+{
+	[HttpGet]
+	public ActionResult<List<User>> GetUsers()
+	{
+		var users = new List<User>()
+		{
+			new User()
+			{
+				Id = 1,
+				Nickname = "Nickname",
+				Password = "password",
+				DateOfBirth = DateTime.Now,
+				Description = "test"
+			},
+			new User()
+			{
+				Id = 1,
+				Nickname = "Nickname2",
+				Password = "12345678",
+				DateOfBirth = DateTime.Now,
+				Description = "test"
+			}
+		};
+		return Ok(users);
+	}
+	[HttpGet("{nickname}")]
+	public ActionResult<User> GetUserByNickname(string nickname)
+	{
+		var user = new User()
+		{
+			Id = new Random().Next(1, 100),
+			Nickname = nickname,
+			Password = "password",
+			DateOfBirth = DateTime.Now,
+			Description = "test"
+		};
+		return Ok(user);
+	}
+	[HttpPost]
+	public ActionResult<User> AddUser(User user)
+	{
+		user.Id = new Random().Next(1, 100);
+		return Created($"users/{user.Id}",user);
+	}
+}
---
+++ Lesson5/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace Lesson5.Models;
+
+public class User
+{
+	public int Id { get; set; }
+	public string Nickname { get; set; }
+	public string Password { get; set; }
+	public DateTime DateOfBirth { get; set; }
+	public string Description { get; set; }
+}
